db0af93f41675b1e65000e99456e60e33fc8e8ac
Anant Mishra <anant.mishra.student@gmail.com>
2018-05-25 00:05:45 +0530

    added main display

--- app/src/main/java/com/ids/team3/messmanager/LoginPage.java
+++ app/src/main/java/com/ids/team3/messmanager/LoginPage.java
@@ -1,13 +1,35 @@
 package com.ids.team3.messmanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class LoginPage extends AppCompatActivity {
+    private Button mBtLaunchActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_login_page);
+        mBtLaunchActivity = (Button) findViewById(R.id.bt_launch_activity);
+
+        mBtLaunchActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                launchActivity();
+            }
+        });
+
+    }
+    private void launchActivity() {
+
+        Intent intent = new Intent(this, MainDisplay.class);
+        startActivity(intent);
     }
 }
