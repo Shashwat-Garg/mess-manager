243b0940d9c40c6eab3aadd896ac39b40ecae27e
Anant Mishra <anant.mishra.student@gmail.com>
2018-05-28 21:54:04 +0530

    added mess allotment activity and changes in main display

--- app/src/main/java/com/ids/team3/messmanager/MainDisplay.java
+++ app/src/main/java/com/ids/team3/messmanager/MainDisplay.java
@@ -1,7 +1,10 @@
 package com.ids.team3.messmanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainDisplay extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainDisplay extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_display);
+
+        Button MessAllotBtn = (Button) findViewById(R.id.mess_allot_btn);
+        MessAllotBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                launchActivity();
+            }
+        });
+    }
+
+    private void launchActivity() {
+
+        Intent intent = new Intent(this, MessAllot.class);
+        startActivity(intent);
     }
 }
