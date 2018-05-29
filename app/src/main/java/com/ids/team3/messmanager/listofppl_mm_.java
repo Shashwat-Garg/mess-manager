a4fbbc048e07c9fd942fed0f321fe758133ea1c1
palakgupta889 <palakgupta889@gmail.com>
2018-05-29 11:31:24 +0530

    added MessManager Activities

---
+++ app/src/main/java/com/ids/team3/messmanager/listofppl_mm_.java
@@ -0,0 +1,21 @@
+package com.ids.team3.messmanager;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class listofppl_mm_ extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listofppl_mm_);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
---
+++ app/src/main/java/com/ids/team3/messmanager/vieworders.java
@@ -0,0 +1,21 @@
+package com.ids.team3.messmanager;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+/*import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.widget.TextView;*/
+
+
+public class vieworders extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activities_vieworders);
+        /*SpannableStringBuilder str = new SpannableStringBuilder("Your awesome text");
+        str.setSpan(new android.text.style.StyleSpan(android.graphics.Typeface.BOLD),0,,Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        TextView tv = findViewById(R.id.bold);
+        tv.setText(str);*/
+    }
+}
